042462b2cb24e06014a83f7dcdc01ed962871a1f
churrowo <li.cheryl.2706@gmail.com>
2020-08-24 10:16:30 -0600

    removed excess files
    no need for black icons

--- store.js
+++ store.js
@@ -2,5 +2,7 @@ var removeCartItemBtn = document.getElementsByClassName('btn-danger');
 console.log(removeCartItemBtn);
 for (var i = 0; i < removeCartItemBtn.length; i++){
     var button = removeCartItemBtn[i];
-    button.addEventListener('click')
+    button.addEventListener('click', function(){
+        console.log('clicked');
+    });
 }
